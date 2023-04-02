d654c374adf1c0dc1933d186759cfad431b25c32
Kundan <ntamsoy662gmail.com>
2023-04-02 15:28:09 +0530

    final clock

--- clock.js
+++ clock.js
@@ -26,12 +26,20 @@ hh = document.getElementById('hh');
 mm = document.getElementById('mm');
 ss = document.getElementById('ss');
 
-document.getElementById('hours').innerHTML=hours;
+hr_dot = document.querySelector('.hr_dot');
+mn_dot = document.querySelector('.mn_dot');
+sc_dot = document.querySelector('.sc_dot');
+
+document.getElementById('hours').innerHTML=hours /*+ '<br><span>hours</span></br>';*/
 document.getElementById('minute').innerHTML=minute;
 document.getElementById('second').innerHTML=second;
 
 hh.style.strokeDashoffset = 440 - (440 * hours) / 12;
 mm.style.strokeDashoffset = 440 - (440 * minute) / 60;
 ss.style.strokeDashoffset = 440 - (440 * second) / 60;
+
+hr_dot.style.transform = `rotate(${hours * 30}deg)`;
+mn_dot.style.transform = `rotate(${minute * 6}deg)`;
+sc_dot.style.transform = `rotate(${second * 6}deg)`;
 
 }, 1000);
